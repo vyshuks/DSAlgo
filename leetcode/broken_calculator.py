b265466d129548c597f080607fa8cb87045019f2
vysakh <vyshuks@gmail.com>
2022-05-04 01:25:19 +0530

    Broken Calculator

---
+++ leetcode/broken_calculator.py
@@ -0,0 +1,15 @@
+"""
+https://leetcode.com/problems/broken-calculator/
+"""
+
+class Solution:
+    def brokenCalc(self, startValue: int, target: int) -> int:
+        ans = 0
+        while target > startValue:
+            ans+=1
+            if target%2 == 0:
+                target = target//2
+            else:
+                target+=1
+        return ans + (startValue-target)
+        
