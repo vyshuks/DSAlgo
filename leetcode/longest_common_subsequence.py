14abc2a9486bfee5c723c356450a2dccf6c90bb0
vysakh <vyshuks@gmail.com>
2022-05-17 04:47:02 +0530

    Longest Common Subsequence

---
+++ leetcode/longest_common_subsequence.py
@@ -0,0 +1,17 @@
+"""
+https://leetcode.com/problems/longest-common-subsequence/
+"""
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+        L = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                
+                if text1[i-1] == text2[j-1]:
+                    L[i][j] = L[i-1][j-1] + 1
+                else:
+                    L[i][j] = max(L[i-1][j], L[i][j-1])
+        return L[m][n]
