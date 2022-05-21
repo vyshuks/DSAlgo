fb629a139c6c2da75f7d80c0e38a2e3313f2b203
vysakh <vyshuks@gmail.com>
2022-05-22 01:13:07 +0530

    Missing Number

--- leetcode/missing_number.py
+++ leetcode/missing_number.py
@@ -1,4 +1,11 @@
 """
 https://leetcode.com/problems/missing-number/
 """
-
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        actual_tot = ((n)*(n+1))//2
+        current_tot = sum(nums)
+        
+        return actual_tot-current_tot
+        
