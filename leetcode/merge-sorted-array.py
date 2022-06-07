d358f86ecc914251c860fe8c628506b71b1784d6
vysakh <vyshuks@gmail.com>
2022-06-07 08:12:51 +0530

    Merge Sorted Array

---
+++ leetcode/merge-sorted-array.py
@@ -0,0 +1,35 @@
+"""
+https://leetcode.com/problems/merge-sorted-array/
+"""
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i,j,k=0,0,0,
+        
+        temp = nums1.copy()
+        
+        while i<m and j<n:
+            if temp[i]<nums2[j]:
+                nums1[k] = temp[i]
+                i+=1
+                k+=1
+            else:
+                nums1[k] = nums2[j]
+                j+=1
+                k+=1
+        
+        while i<m:
+            nums1[k] = temp[i]
+            i+=1
+            k+=1
+        while j<n:
+            nums1[k] = nums2[j]
+            j+=1
+            k+=1
+            
+        
+            
+        
