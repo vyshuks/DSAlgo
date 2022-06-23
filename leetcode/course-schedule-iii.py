4ffdf03119333acb4bb263d788377256a8baf60c
vysakh <vyshuks@gmail.com>
2022-06-23 11:24:00 +0530

    Course Schedule III

---
+++ leetcode/course-schedule-iii.py
@@ -0,0 +1,24 @@
+import heapq
+
+class Solution:
+    def scheduleCourse(self, courses: List[List[int]]) -> int:
+        courses.sort(key=lambda c: c[-1])
+        heap = []
+        heapq.heapify(heap)
+        maxtime = 0
+        for course in courses:
+            time_need, last_day = course
+            heapq.heappush(heap, -time_need)
+            maxtime+= time_need
+
+            if maxtime > last_day:
+                max_dur = -1 * heapq.heappop(heap)
+                maxtime-= max_dur
+
+        return len(heap)
+
+
+            
+        
+        
+        
